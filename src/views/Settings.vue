02a47d4559a239ff6c9f63a5780bea58b9eb837b
pakidaxed <jucius.tomas@gmail.com>
2020-07-30 19:01:53 +0300

    sudeti errorrai

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,13 +1,36 @@
 <template>
     <div class="settings">
         <h1>SETTINGS:</h1>
-        <Notification :display="error" :type="type" :message="errorMessage" />
+        <Notification :display="error" :type="type" :message="errorMessage"/>
         <form @submit.prevent="changeName">
-            <div class="field">
-                <label class="label" id="label-name">Your name</label>
-                <div class="control">
-                    <input class="input" v-model="userName" type="text" :placeholder="user.name"/>
+            <div class="field-body">
+                <div class="field">
+                    <label class="label" id="label-name">Your name</label>
+                    <div class="control">
+                        <input class="input" v-model="userName" type="text" :placeholder="user.name"/>
+                    </div>
                 </div>
+                <div class="field">
+                    <label class="label" id="age-name">Your age</label>
+                    <div class="control">
+                        <input class="input" v-model="userAge" type="number" :placeholder="user.age"/>
+                    </div>
+                </div>
+            </div>
+
+            <div class="control">
+                <label class="radio">
+                    <input type="radio" name="gender" v-model="userGender" value="Male">
+                    Male
+                </label>
+                <label class="radio">
+                    <input type="radio" name="gender" v-model="userGender" value="Female">
+                    Female
+                </label>
+                <label class="radio">
+                    <input type="radio" name="gender" v-model="userGender" value="Other">
+                    Other
+                </label>
             </div>
             <div class="control">
                 <button class="button is-primary" @click="changeName">Change name</button>
@@ -24,11 +47,13 @@
 
     export default {
         name: "Settings",
-        components: { Notification },
+        components: {Notification},
         data() {
             return {
                 user: [],
                 userName: "",
+                userAge: "",
+                userGender: "",
                 type: "",
                 error: false,
                 errorMessage: ""
@@ -41,20 +66,20 @@
                     .collection("users")
                     .doc(firebase.auth().currentUser.uid)
                     .update({
-                        age: 33,
-                        name: this.userName
+                        age: this.userAge,
+                        name: this.userName,
+                        gender: this.userGender
                     })
 
-                .then(() => {
-                    this.type = "is-primary"
-                    this.error = true
-                    this.errorMessage = "Jusu vardas sekmingai pakeistas i " + this.userName
-                }, error => {
-                    this.type = "is-danger"
-                    this.error = true
-                    this.errorMessage = "nu nepavyko"
-                    console.log(error.message)
-                })
+                    .then(() => {
+                        this.type = "is-primary"
+                        this.error = true
+                        this.errorMessage = `Jusu vardas sekmingai pakeistas i ${this.userName}, amzius ${this.userAge}, lytis ${this.userGender}.`
+                    }, error => {
+                        this.type = "is-danger"
+                        this.error = true
+                        this.errorMessage = error.message
+                    })
 
 
             }
@@ -65,7 +90,13 @@
                 .firestore()
                 .collection("users").doc(firebase.auth().currentUser.uid)
                 .get()
-                .then((doc) => this.user = doc.data())
+                .then((doc) => this.user = doc.data(),
+                    (error) => {
+                        this.error = true;
+                        this.type = "is-danger";
+                        this.errorMessage = error.message;
+                    })
+
         }
     }
 </script>
